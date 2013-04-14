13fe1af26eafd238ea7cadc91ed4d4d1f623c9a2
Cloudmanic Labs, LLC <github@cloudmanic.com>
2013-04-14 00:16:26 -0700

    Fixed date info

--- cloudjs.js
+++ cloudjs.js
@@ -834,7 +834,7 @@ cloudjs.run_poll = function (name)
 Handlebars.registerHelper('dateFormat', function(context, block) {
   if (window.moment) {
     var f = block.hash.format || "MMM Do, YYYY";
-    return moment(Date(context)).format(f);
+    return moment(context).format(f);
   }else{
     return context;   //  moment plugin not available. return data as is.
   };
